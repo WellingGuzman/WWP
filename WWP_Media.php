5f1a9c597320f5d2708494a2439e046f66ef365a
Welling Guzman <WellingGuzman@users.noreply.github.com>
2015-01-01 23:07:24 -0400

    getFileType

--- WWP_Media.php
+++ WWP_Media.php
@@ -5,4 +5,8 @@ class WWP_Media {
   function static getMediaURLByID($id) {
     return wp_get_attachment_url( get_post_thumbnail_id( $id ) );
   }
+  
+  function static getFileType($filename) {
+    return wp_check_filetype( basename( $filename ), null );
+  }
 }
