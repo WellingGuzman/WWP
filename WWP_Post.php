e84c3ec3d4e43c7413c4222589ec811b3dff7658
Welling Guzman <WellingGuzman@users.noreply.github.com>
2015-07-15 21:20:26 -0400

    Update WWP_Post.php

--- WWP_Post.php
+++ WWP_Post.php
@@ -3,4 +3,17 @@
 
 class WWP_Post
 {
+  public static function getPostBySlug($slug, $postType = 'post')
+  {
+    $args = array(
+      'name'        => $slug,
+      'post_type'   => $postType,
+      'post_status' => 'publish',
+      'numberposts' => 1
+    );
+    
+    $posts = get_posts($args);
+    
+    return $posts;
+  }
 }
